d176c0feefdbe45f2f47a0f520f519dbcf20ba90
Dragos Rotaru <7482137+dragosrotaru@users.noreply.github.com>
2025-03-05 19:18:01 -0500

    suspense

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,14 +3,16 @@ import ThemeSettings from './_components/theme'
 import ReferralSettings from './_components/referral'
 import EmailSettings from './_components/email'
 import ProfileSettings from './_components/profile'
-
+import { Suspense } from 'react'
 export default function UserSettingsPage() {
   return (
     <div className="max-w-2xl mx-auto">
       <h1 className="text-3xl font-bold mb-8">User Settings</h1>
 
       <div className="space-y-6">
-        <EmailSettings />
+        <Suspense>
+          <EmailSettings />
+        </Suspense>
         <ProfileSettings />
         <ReferralSettings />
         <ThemeSettings />
